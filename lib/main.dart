680aac739ce4a04116e412ef25c9092f1c198061
Rasya Febrian Dema <rasyadema07@gmail.com>
2025-10-08 18:13:02 +0800

    checkpoint_3

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:app_links/app_links.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  StreamSubscription? _sub;
+  String _status = 'Waiting for link...';
+  final AppLinks _appLinks = AppLinks();
+
+  @override
+  void initState() {
+    super.initState();
+    initAppLinks();
+  }
+
+  Future<void> initAppLinks() async {
+    // 1) Handle initial link (cold start)
+    try {
+      final dynamic initialLink = await _appLinks.getInitialAppLink();
+      if (initialLink != null) {
+        if (initialLink is Uri) {
+          _handleIncomingLink(initialLink);
+        } else if (initialLink is String) {
+          _handleIncomingLinkFromString(initialLink);
+        }
+      }
+    } catch (err) {
+      setState(() => _status = 'Failed to get initial link: $err');
+    }
+
+    // 2) Handle links while the app is running
+    try {
+      _sub = _appLinks.uriLinkStream.listen(
+        (uri) {
+          _handleIncomingLink(uri);
+        },
+        onError: (err) {
+          setState(() => _status = 'Failed to receive link: $err');
+        },
+      );
+    } catch (err) {
+      setState(() => _status = 'Failed to subscribe to link stream: $err');
+    }
+  }
+
+  void _handleIncomingLink(Uri uri) {
+    setState(() => _status = 'Received link: $uri');
+
+    if (uri.host == 'details') {
+      // Example link: myapp://details/42
+      final id = uri.pathSegments.isNotEmpty ? uri.pathSegments[0] : 'unknown';
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => DetailScreen(id: id)),
+      );
+    }
+  }
+
+  void _handleIncomingLinkFromString(String link) {
+    try {
+      final uri = Uri.parse(link);
+      _handleIncomingLink(uri);
+    } catch (err) {
+      setState(() => _status = 'Bad link received: $link');
+    }
+  }
+
+  @override
+  void dispose() {
+    _sub?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Deep Link Demo',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomeScreen(),
-        '/details': (context) => DetailScreen(),
-      },
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Home')),
-      body: Center(
-        child: Text('Welcome! Try opening myapp://details/42'),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Home')),
+        body: Center(child: Text(_status)),
       ),
     );
   }
 }
 
 class DetailScreen extends StatelessWidget {
+  final String id;
+  const DetailScreen({required this.id});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Details')),
-      body: Center(child: Text('You are on the Detail screen!')),
+      body: Center(child: Text('You opened item ID: $id')),
     );
   }
 }
